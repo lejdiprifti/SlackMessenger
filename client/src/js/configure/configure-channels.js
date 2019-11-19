ff0a510ece96d0667008f45c25a77b30fd3d730c
lejdiprifti <priftilejdi@gmail.com>
2019-11-19 17:23:04 +0100

    add channels and update added channels

--- client/src/js/configure/configure-channels.js
+++ client/src/js/configure/configure-channels.js
@@ -1,10 +1,17 @@
 import { getToken } from '../login/loginboard.js'
 const template = document.createElement('template')
 template.innerHTML = `
+<link rel="stylesheet" href="../../css/configure/configure.css" />
 <div id="channelsDiv">
+<div id="channels"> 
+<template>
+<p></p>
+</template>
+</div>
 <label>Add channels</label>
 <input id="input" type="text" placeholder="Channel" required/>
 <input id="submit" type="submit" value="Add" />
+
 </div>
 `
 
@@ -17,15 +24,16 @@ export class ConfigureChannels extends window.HTMLElement {
   }
 
   connectedCallback () {
+    this.addChannels()
     this.getChannels()
   }
 
-  async getChannels () {
+  async addChannels () {
     const submit = this.shadowRoot.querySelector('#submit')
     const input = this.shadowRoot.querySelector('#input')
     console.log(getToken())
     submit.addEventListener('click', async event => {
-      const response = await fetch('http://localhost:8080/channels', {
+      await fetch('http://localhost:8080/channels', {
         method: 'POST',
         headers: {
           'Content-type': 'application/json',
@@ -35,8 +43,31 @@ export class ConfigureChannels extends window.HTMLElement {
           channelId: input.value
         })
       })
-      console.log(response)
+      this.getChannels()
     })
+  }
+
+  async getChannels () {
+    this.shadowRoot.querySelectorAll('p').forEach(obj => {
+      obj.innerHTML = ''
+    })
+    const response = await fetch('http://localhost:8080/channels', {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: 'Bearer ' + getToken()
+      }
+    })
+    const answer = await response.json()
+    let paragraph = null
+    const template = this.shadowRoot.querySelector('#channels template').content
+    const channels = document.importNode(template, false)
+    answer.forEach(obj => {
+      paragraph = document.importNode(template.firstElementChild, true)
+      paragraph.innerText = obj.channelId
+      channels.appendChild(paragraph)
+    })
+    this.shadowRoot.querySelector('#channels').appendChild(channels)
   }
 }
 
